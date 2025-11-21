02ffb437ecb872fb6e9852f0ad321831893c958a
effinmr <effinmr@gmail.com>
2025-11-21 15:54:01 +0530

    Update PlaylistAdapter.kt

--- app/src/main/java/code/name/monkey/retromusic/adapter/playlist/PlaylistAdapter.kt
+++ app/src/main/java/code/name/monkey/retromusic/adapter/playlist/PlaylistAdapter.kt
@@ -121,7 +121,16 @@ class PlaylistAdapter(
             holder.image?.setPadding(activity.dipToPix(8F).toInt())
             holder.image?.setImageDrawable(getIconRes())
         } else {
-            loadPlaylistImage(playlist)
+            val customImageFile = CustomPlaylistImageUtil.getFile(playlist.playlistEntity)
+            val imageModel: Any = if (customImageFile.exists()) {
+                customImageFile
+            } else {
+                PlaylistPreview(playlist)
+            }
+            Glide.with(activity)
+                .load(imageModel)
+                .playlistOptions()
+                .into(holder.image!!)
         }
         val dragHandle = holder.itemView.findViewById<View>(R.id.dragHandle)
         dragHandle?.setOnTouchListener { _, event ->
@@ -144,19 +153,6 @@ class PlaylistAdapter(
         mutableList.add(toPosition, movedItem)
         dataSet = mutableList
         notifyItemMoved(fromPosition, toPosition)
-    }
-
-    private fun loadPlaylistImage(playlist: PlaylistEntity) {
-        val customImageFile = CustomPlaylistImageUtil.getFile(playlist.playlistEntity)
-        val imageModel: Any = if (customImageFile.exists()) {
-            customImageFile
-        } else {
-            PlaylistPreview(playlist)
-        }
-        Glide.with(activity)
-            .load(imageModel)
-            .playlistOptions()
-            .into(holder.image!!)
     }
 
     override fun getItemCount(): Int {
