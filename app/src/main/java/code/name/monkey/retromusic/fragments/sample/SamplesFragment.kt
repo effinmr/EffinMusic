9842d40d880297f20dbca38556bd9f3b73503824
Dhruvit Pokharna <10249734+dhruvitpokharna@users.noreply.github.com>
2025-11-23 02:33:25 +0530

    Update SamplesFragment.kt

--- app/src/main/java/code/name/monkey/retromusic/fragments/sample/SamplesFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/sample/SamplesFragment.kt
@@ -154,6 +154,11 @@ class SamplesFragment : AbsPlayerFragment(R.layout.fragment_samples),
         _binding = null
     }
 
+    override onDestroy() {
+        super.onDestroy()
+        MusicPlayerRemote.clearQueue()
+    }
+
    private fun updateArtistImage() {
         val song = MusicPlayerRemote.currentSong
         val ids = song.artistIds?.split(",")?.mapNotNull { it.trim().toLongOrNull() } ?: emptyList()
