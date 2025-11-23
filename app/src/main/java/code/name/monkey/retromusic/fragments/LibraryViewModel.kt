9def3e1cd5018be0b3fcc177afb139f08789799b
effinmr <effinmr@gmail.com>
2025-11-23 12:44:13 +0530

    Update LibraryViewModel.kt

--- app/src/main/java/code/name/monkey/retromusic/fragments/LibraryViewModel.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/LibraryViewModel.kt
@@ -55,6 +55,7 @@ class LibraryViewModel(
     private val _paletteColor = MutableLiveData<Int>()
     private val home = MutableLiveData<List<Home>>()
     private val suggestions = MutableLiveData<List<Song>>()
+    private val samples = MutableLiveData<List<Song>>()
     private val albums = MutableLiveData<List<Album>>()
     private val songs = MutableLiveData<List<Song>>()
     val artists = MutableLiveData<List<Artist>>()
@@ -78,6 +79,7 @@ class LibraryViewModel(
             fetchHomeSections()
             awaitAll(
                 async { fetchSuggestions() },
+                async { fetchSamples() },
                 async { fetchSongs() },
                 async { fetchAlbums() },
                 async { fetchArtists() },
@@ -102,6 +104,8 @@ class LibraryViewModel(
     fun getHome(): LiveData<List<Home>> = home
 
     fun getSuggestions(): LiveData<List<Song>> = suggestions
+
+    fun getSamples(): LiveData<List<Song>> = samples
 
     fun getFabMargin(): LiveData<Int> = fabMargin
 
@@ -137,6 +141,12 @@ class LibraryViewModel(
 
     private suspend fun fetchSuggestions() {
         suggestions.postValue(repository.suggestions())
+    }
+
+    private suspend fun fetchSamples() {
+        val all = repository.suggestions()
+        val fastLoad = if (all.size > 100) all.take(100) else all
+        samples.postValue(fastLoad)
     }
 
     fun search(query: String?, filter: Filter) =
@@ -204,6 +214,7 @@ class LibraryViewModel(
             Playlists -> fetchPlaylists()
             Genres -> fetchGenres()
             Suggestions -> fetchSuggestions()
+            Samples -> fetchSamples()
         }
     }
 
@@ -461,5 +472,6 @@ enum class ReloadType {
     HomeSections,
     Playlists,
     Genres,
-    Suggestions
+    Suggestions,
+    Samples
 }
