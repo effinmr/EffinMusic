0261b0c79b07fa46d5d4eb82ec201a48c566debe
Dhruvit <effinmr@gmail.com>
2026-05-18 03:12:02 +0530

    Update ArtistDetailsAdapter.kt

--- app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
+++ app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
@@ -23,7 +23,7 @@ import code.name.monkey.retromusic.databinding.ItemArtistStatsBinding
 import code.name.monkey.retromusic.fragments.artists.ArtistItem
 import code.name.monkey.retromusic.glide.BlurTransformation
 import code.name.monkey.retromusic.glide.RetroGlideExtension
-import code.name.monkey.retromusic.glide.RetroGlideExtension.samplesCoverOptions
+import code.name.monkey.retromusic.glide.RetroGlideExtension.samplesImageOptions
 import code.name.monkey.retromusic.glide.RetroGlideExtension.artistImageOptions
 import code.name.monkey.retromusic.glide.RetroGlideExtension.asBitmapPalette
 import code.name.monkey.retromusic.glide.SingleColorTarget
@@ -217,7 +217,7 @@ class ArtistDetailsAdapter(
             
             Glide.with(binding.image.context)
                 .load(model)
-                .samplesCoverOptions(song)
+                .samplesImageOptions(song)
                 .transform(
                     BlurTransformation.Builder(binding.image.context)
                         .blurRadius(8f)
