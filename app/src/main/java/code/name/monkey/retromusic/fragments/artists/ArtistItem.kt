08835e3b29db6ad158382118e8258be09f5333d3
Dhruvit <effinmr@gmail.com>
2026-05-17 20:34:13 +0530

    Update ArtistItem.kt

--- app/src/main/java/code/name/monkey/retromusic/fragments/artists/ArtistItem.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/artists/ArtistItem.kt
@@ -7,6 +7,7 @@ import code.name.monkey.retromusic.model.Song
 
 sealed class ArtistItem {
     data class Header(val artist: Artist) : ArtistItem()
+    data class Samples(val artist: Artist) : ArtistItem()
     data class Albums(val albums: List<Album>) : ArtistItem()
     data class SongsHeader(val title: String) : ArtistItem()
     data class SongItem(val song: Song) : ArtistItem()
