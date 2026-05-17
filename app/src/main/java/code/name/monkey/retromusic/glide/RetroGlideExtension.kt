cbb114df2b749588b58d5970316fb1bfdefe1519
Dhruvit <effinmr@gmail.com>
2026-05-18 02:59:02 +0530

    Add samplesCoverOptions method to RequestBuilder

--- app/src/main/java/code/name/monkey/retromusic/glide/RetroGlideExtension.kt
+++ app/src/main/java/code/name/monkey/retromusic/glide/RetroGlideExtension.kt
@@ -135,6 +135,13 @@ object RetroGlideExtension {
             .signature(createSignature(song))
     }
 
+    fun <T> RequestBuilder<T>.samplesCoverOptions(
+        song: Song
+    ): RequestBuilder<T> {
+        return diskCacheStrategy(DEFAULT_DISK_CACHE_STRATEGY)
+            .signature(createSignature(song))
+    }
+
     fun <T> RequestBuilder<T>.userProfileOptions(
         file: File,
         context: Context
