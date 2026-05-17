817e3e57ff6276cc2be0a1fe4672d37789333432
Dhruvit <effinmr@gmail.com>
2026-05-17 20:35:24 +0530

    Update ArtistDetailsAdapter.kt

--- app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
+++ app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
@@ -203,7 +203,7 @@ class ArtistDetailsAdapter(
             binding.artistSamplesContainer.setOnClickListener {
                 binding.root.findNavController().navigate(
                     R.id.action_sample,
-                    bundleOf(EXTRA_ARTIST_ID to artist.id)
+                    bundleOf("extra_artist_id" to artist.id)
                 )
             }
         }
