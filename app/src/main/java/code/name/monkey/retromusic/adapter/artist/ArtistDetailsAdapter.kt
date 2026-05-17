b71b32110c1a8937e513554f809bc7d74c8357da
Dhruvit <effinmr@gmail.com>
2026-05-18 00:35:38 +0530

    Update ArtistDetailsAdapter.kt

--- app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
+++ app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
@@ -22,6 +22,7 @@ import code.name.monkey.retromusic.databinding.ItemArtistSongBinding
 import code.name.monkey.retromusic.databinding.ItemArtistStatsBinding
 import code.name.monkey.retromusic.fragments.artists.ArtistItem
 import code.name.monkey.retromusic.glide.RetroGlideExtension
+import code.name.monkey.retromusic.glide.RetroGlideExtension.albumCoverOptions
 import code.name.monkey.retromusic.glide.RetroGlideExtension.artistImageOptions
 import code.name.monkey.retromusic.glide.RetroGlideExtension.asBitmapPalette
 import code.name.monkey.retromusic.glide.SingleColorTarget
