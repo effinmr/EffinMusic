8b9e309e20383f9c7c5790cdcef0af50c7cf0d6f
Dhruvit <effinmr@gmail.com>
2026-05-18 04:11:24 +0530

    Update SamplesFragment.kt

--- app/src/main/java/code/name/monkey/retromusic/fragments/sample/SamplesFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/sample/SamplesFragment.kt
@@ -193,7 +193,7 @@ class SamplesFragment : AbsPlayerFragment(R.layout.fragment_samples),
                 if (artist.id != -1L) {
                     Glide.with(requireActivity())
                         .load(RetroGlideExtension.getArtistModel(artist))
-                        .albumCoverOptions(artist)
+                        .artistImageOptions(artist)
                         .override(200, 200)
                         .into(binding.artistImage)
                 }
