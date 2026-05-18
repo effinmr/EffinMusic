50f8bee39edee858b39626aa3cc6f5a6679f482d
Dhruvit Pokharna <10249734+dhruvitpokharna@users.noreply.github.com>
2026-05-18 18:53:56 +0530

    Update SamplesFragment.kt

--- app/src/main/java/code/name/monkey/retromusic/fragments/sample/SamplesFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/sample/SamplesFragment.kt
@@ -19,8 +19,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.Toolbar
-import androidx.transition.Slide
-import android.view.Gravity
 import code.name.monkey.appthemehelper.util.ToolbarContentTintHelper
 import code.name.monkey.retromusic.R
 import code.name.monkey.retromusic.databinding.FragmentSamplesBinding
@@ -51,8 +49,6 @@ class SamplesFragment : AbsPlayerFragment(R.layout.fragment_samples),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enterTransition = Slide(Gravity.BOTTOM)
-        exitTransition = Slide(Gravity.BOTTOM)
         progressViewUpdateHelper = MusicProgressViewUpdateHelper(this)
     }
 
