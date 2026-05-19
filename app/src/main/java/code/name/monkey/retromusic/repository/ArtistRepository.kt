72f9544c21a9d3cb27988ebceb81486579052c33
Dhruvit <effinmr@gmail.com>
2026-05-19 16:29:28 +0530

    Update ArtistRepository.kt

--- app/src/main/java/code/name/monkey/retromusic/repository/ArtistRepository.kt
+++ app/src/main/java/code/name/monkey/retromusic/repository/ArtistRepository.kt
@@ -126,13 +126,21 @@ class RealArtistRepository(
     override fun albumArtist(artistName: String): Artist {
         if (artistName == Artist.VARIOUS_ARTISTS_DISPLAY_NAME) {
             // Get Various Artists
-            val songs = songRepository.songs(
-                songRepository.makeSongCursor(
-                    null,
-                    null,
-                    getSongLoaderSortOrder()
+            val songs = if (!PreferenceUtil.fixYear) {
+                songRepository.songs(
+                    songRepository.makeSongCursor(
+                        null,
+                        null,
+                        getSongLoaderSortOrder()
+                    )
                 )
-            )
+            } else {
+                songRepository.songs(PreferenceUtil.hideDuplicateSongs)
+                .filter { song -> 
+                    song.albumArtist?.trim() == artistName
+                }
+            }
+                
             val albums = albumRepository.splitIntoAlbums(songs)
                 .filter { it.albumArtist == Artist.VARIOUS_ARTISTS_DISPLAY_NAME }
             return Artist(Artist.VARIOUS_ARTISTS_ID, albums, true)
