b2a2906723b9e9934e0e1e98f57255069b27c68a
Dhruvit <effinmr@gmail.com>
2026-05-21 00:34:52 +0530

    Add import for isGone extension function

--- app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
+++ app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistDetailsAdapter.kt
@@ -3,6 +3,7 @@ package code.name.monkey.retromusic.adapter.artist
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
+import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
