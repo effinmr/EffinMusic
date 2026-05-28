5ef07bf6f157e66de66c47cf7d99078bee2af504
Dhruvit <effinmr@gmail.com>
2026-05-28 10:37:02 +0530

    Update PlaybackManager.kt

--- app/src/main/java/code/name/monkey/retromusic/service/PlaybackManager.kt
+++ app/src/main/java/code/name/monkey/retromusic/service/PlaybackManager.kt
@@ -112,14 +112,14 @@ class PlaybackManager(val context: Context) {
      * @return Whether switched playback
      */
     fun maybeSwitchToCrossFade(crossFadeDuration: Int): Boolean {
-        /* Switch to ExoPlayer if CrossFade duration is 0 and
-                Playback is not an instance of ExoPlayer */
-        if (playback !is ExoPlayer && crossFadeDuration == 0) {
+        /* Switch to RetroExoPlayer if CrossFade duration is 0 and
+                Playback is not an instance of RetroExoPlayer */
+        if (playback !is RetroExoPlayer && crossFadeDuration == 0) {
             if (playback != null) {
                 playback?.release()
             }
             playback = null
-            playback = ExoPlayer(context)
+            playback = RetroExoPlayer(context)
             return true
         } else if (playback !is CrossFadePlayer && crossFadeDuration > 0) {
             if (playback != null) {
@@ -182,9 +182,9 @@ class PlaybackManager(val context: Context) {
     }
 
     private fun createLocalPlayback(): Playback {
-        // Set ExoPlayer when crossfade duration is 0 i.e. off
+        // Set RetroExoPlayer when crossfade duration is 0 i.e. off
         return if (PreferenceUtil.crossFadeDuration == 0) {
-            ExoPlayer(context)
+            RetroExoPlayer(context)
         } else {
             CrossFadePlayer(context)
         }
