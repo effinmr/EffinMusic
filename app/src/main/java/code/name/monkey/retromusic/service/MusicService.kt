b24e149c56f8f0472f8854e85291ee655eba03ac
Dhruvit <effinmr@gmail.com>
2026-05-28 12:05:24 +0530

    Update MusicService.kt

--- app/src/main/java/code/name/monkey/retromusic/service/MusicService.kt
+++ app/src/main/java/code/name/monkey/retromusic/service/MusicService.kt
@@ -18,7 +18,12 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.bluetooth.BluetoothDevice
-import android.content.*
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.content.pm.ServiceInfo
 import android.database.ContentObserver
@@ -26,9 +31,13 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.media.AudioManager
-import android.os.*
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
+import android.os.Binder
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.IBinder
+import android.os.Looper
+import android.os.PowerManager
 import android.os.PowerManager.WakeLock
 import android.provider.MediaStore
 import android.support.v4.media.MediaBrowserCompat
@@ -46,9 +55,22 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.media.MediaBrowserServiceCompat
 import androidx.preference.PreferenceManager
 import code.name.monkey.appthemehelper.util.VersionUtils
-import code.name.monkey.retromusic.*
+import code.name.monkey.retromusic.ALBUM_ART_ON_LOCK_SCREEN
+import code.name.monkey.retromusic.BLURRED_ALBUM_ART
+import code.name.monkey.retromusic.BuildConfig
+import code.name.monkey.retromusic.CROSS_FADE_DURATION
+import code.name.monkey.retromusic.PLAYBACK_PITCH
+import code.name.monkey.retromusic.PLAYBACK_SPEED
+import code.name.monkey.retromusic.R
+import code.name.monkey.retromusic.TOGGLE_HEADSET
 import code.name.monkey.retromusic.activities.LockScreenActivity
-import code.name.monkey.retromusic.appwidgets.*
+import code.name.monkey.retromusic.appwidgets.AppWidgetBig
+import code.name.monkey.retromusic.appwidgets.AppWidgetCard
+import code.name.monkey.retromusic.appwidgets.AppWidgetCircle
+import code.name.monkey.retromusic.appwidgets.AppWidgetClassic
+import code.name.monkey.retromusic.appwidgets.AppWidgetMD3
+import code.name.monkey.retromusic.appwidgets.AppWidgetSmall
+import code.name.monkey.retromusic.appwidgets.AppWidgetText
 import code.name.monkey.retromusic.auto.AutoMediaIDHelper
 import code.name.monkey.retromusic.auto.AutoMusicProvider
 import code.name.monkey.retromusic.extensions.showToast
@@ -65,8 +87,6 @@ import code.name.monkey.retromusic.providers.HistoryStore
 import code.name.monkey.retromusic.providers.MusicPlaybackQueueStore
 import code.name.monkey.retromusic.providers.SongPlayCountStore
 import code.name.monkey.retromusic.service.notification.PlayingNotification
-import code.name.monkey.retromusic.service.notification.PlayingNotificationClassic
-import code.name.monkey.retromusic.service.notification.PlayingNotificationImpl24
 import code.name.monkey.retromusic.service.playback.Playback
 import code.name.monkey.retromusic.service.playback.Playback.PlaybackCallbacks
 import code.name.monkey.retromusic.util.MusicUtil
@@ -76,11 +96,9 @@ import code.name.monkey.retromusic.util.PreferenceUtil.crossFadeDuration
 import code.name.monkey.retromusic.util.PreferenceUtil.isAlbumArtOnLockScreen
 import code.name.monkey.retromusic.util.PreferenceUtil.isBluetoothSpeaker
 import code.name.monkey.retromusic.util.PreferenceUtil.isBlurredAlbumArt
-import code.name.monkey.retromusic.util.PreferenceUtil.isClassicNotification
 import code.name.monkey.retromusic.util.PreferenceUtil.isHeadsetPlugged
 import code.name.monkey.retromusic.util.PreferenceUtil.isLockScreen
 import code.name.monkey.retromusic.util.PreferenceUtil.isPauseOnZeroVolume
-import code.name.monkey.retromusic.util.PreferenceUtil.isSkipToPrevious
 import code.name.monkey.retromusic.util.PreferenceUtil.playbackPitch
 import code.name.monkey.retromusic.util.PreferenceUtil.playbackSpeed
 import code.name.monkey.retromusic.util.PreferenceUtil.registerOnSharedPreferenceChangedListener
@@ -91,12 +109,18 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.java.KoinJavaComponent.get
-import java.util.*
+import java.util.Objects
+import java.util.Random
 
 
 /**
@@ -387,7 +411,7 @@ class MusicService : MediaBrowserServiceCompat(),
     }
 
     fun back(force: Boolean) {
-        if (songProgressMillis > isSkipToPrevious*1000) {
+        if (songProgressMillis > 2000) {
             seek(0)
         } else {
             playPreviousSong(force)
@@ -546,13 +570,7 @@ class MusicService : MediaBrowserServiceCompat(),
     }
 
     private fun initNotification() {
-        playingNotification = if (VERSION.SDK_INT >= VERSION_CODES.N
-            && !isClassicNotification
-        ) {
-            PlayingNotificationImpl24.from(this, notificationManager!!, mediaSession!!)
-        } else {
-            PlayingNotificationClassic.from(this, notificationManager!!)
-        }
+        playingNotification = PlayingNotification.from(this, notificationManager!!, mediaSession!!)
     }
 
     private val isLastTrack: Boolean
@@ -655,20 +673,6 @@ class MusicService : MediaBrowserServiceCompat(),
             }
 
             ALBUM_ART_ON_LOCK_SCREEN, BLURRED_ALBUM_ART -> updateMediaSessionMetaData(::updateMediaSessionPlaybackState)
-            COLORED_NOTIFICATION -> {
-                playingNotification?.updateMetadata(currentSong) {
-                    playingNotification?.setPlaying(isPlaying)
-                    startForegroundOrNotify()
-                }
-            }
-
-            CLASSIC_NOTIFICATION -> {
-                updateNotification()
-                playingNotification?.updateMetadata(currentSong) {
-                    playingNotification?.setPlaying(isPlaying)
-                    startForegroundOrNotify()
-                }
-            }
 
             TOGGLE_HEADSET -> registerHeadsetEvents()
         }
@@ -707,13 +711,15 @@ class MusicService : MediaBrowserServiceCompat(),
         acquireWakeLock()
         // if there is a timer finished, don't continue
         if (pendingQuit
-            || repeatMode == REPEAT_MODE_NONE && isLastTrack
+            || (repeatMode == REPEAT_MODE_NONE && isLastTrack)
         ) {
-            notifyChange(PLAY_STATE_CHANGED)
+            quit()
             seek(0, false)
             if (pendingQuit) {
                 pendingQuit = false
-                quit()
+            } else if (repeatMode == REPEAT_MODE_NONE && isLastTrack) {
+                position = 0
+                notifyChange(QUEUE_CHANGED)
             }
         } else {
             playNextSong(false)
@@ -811,40 +817,23 @@ class MusicService : MediaBrowserServiceCompat(),
         playSongAt(getNextPosition(force))
     }
 
-    fun playNextSongFrom(force: Boolean, startMs: Int = 30000) {
-        playSongAtFrom(getNextPosition(force), startMs)
-    }
-
     fun playPreviousSong(force: Boolean) {
         playSongAt(getPreviousPosition(force))
     }
 
-    var lastPlayTapTime = 0L
     fun playSongAt(position: Int) {
         // Every chromecast method needs to run on main thread or you are greeted with IllegalStateException
         // So it will use Main dispatcher
         // And by using Default dispatcher for local playback we are reduce the burden of main thread
-
-        val currentTime = System.currentTimeMillis()
-        if (currentTime - lastPlayTapTime < 100) return  // Ignore if tapped too soon again
-        lastPlayTapTime = currentTime
-        
         serviceScope.launch(if (playbackManager.isLocalPlayback) Default else Main) {
             openTrackAndPrepareNextAt(position) { success ->
-                play()
-            }
-        }
-    }
-
-    fun playSongAtFrom(position: Int, startMs: Int = 30000) {
-        serviceScope.launch(if (playbackManager.isLocalPlayback) Default else Main) {
-        // Prepare track but DO NOT PLAY automatically
-            openTrackAndPrepareNextAt(position) { success ->
-                if (!success) return@openTrackAndPrepareNextAt
-
-                seek(startMs, true)
-
-                play()
+                if (success) {
+                    play()
+                } else {
+                    runOnUiThread {
+                        showToast(R.string.unplayable_file)
+                    }
+                }
             }
         }
     }
@@ -853,7 +842,7 @@ class MusicService : MediaBrowserServiceCompat(),
     fun prepareNextImpl() {
         try {
             val nextPosition = getNextPosition(false)
-            playbackManager.setNextDataSource(getSongAt(nextPosition).uri.toString())
+            playbackManager.setNextDataSource(getSongAt(nextPosition).uri)
             this.nextPosition = nextPosition
         } catch (ignored: Exception) {
         }
@@ -884,17 +873,9 @@ class MusicService : MediaBrowserServiceCompat(),
 
         playingNotification?.clear(this)
 
-        mediaSession?.isActive = false // Deactivate media session immediately
-        mediaSession?.setPlaybackState(
-            PlaybackStateCompat.Builder()
-                .setState(PlaybackStateCompat.STATE_STOPPED, 0, 1.0f)
-                .setActions(0) // No actions available when stopped
-                .build()
-        )
-
-
         stopSelf()
     }
+
     private fun releaseWakeLock() {
         if (wakeLock!!.isHeld) {
             wakeLock?.release()
@@ -1076,7 +1057,7 @@ class MusicService : MediaBrowserServiceCompat(),
                 MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER,
                 (getPosition() + 1).toLong()
             )
-            .putLong(MediaMetadataCompat.METADATA_KEY_YEAR, song.year?.toLongOrNull() ?: 0L)
+            .putLong(MediaMetadataCompat.METADATA_KEY_YEAR, song.year.toLong())
             .putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, null)
             .putLong(MediaMetadataCompat.METADATA_KEY_NUM_TRACKS, playingQueue.size.toLong())
 
@@ -1307,7 +1288,12 @@ class MusicService : MediaBrowserServiceCompat(),
 
     private fun registerHeadsetEvents() {
         if (!headsetReceiverRegistered && isHeadsetPlugged) {
-            ContextCompat.registerReceiver(this, headsetReceiver, headsetReceiverIntentFilter, ContextCompat.RECEIVER_EXPORTED)
+            ContextCompat.registerReceiver(
+                this,
+                headsetReceiver,
+                headsetReceiverIntentFilter,
+                ContextCompat.RECEIVER_EXPORTED
+            )
             headsetReceiverRegistered = true
         }
     }
@@ -1371,9 +1357,11 @@ class MusicService : MediaBrowserServiceCompat(),
             )
                 .build()
         )
+        val shuffleIcon =
+            if (getShuffleMode() == SHUFFLE_MODE_NONE) R.drawable.ic_shuffle_off_circled else R.drawable.ic_shuffle_on_circled
         stateBuilder.addCustomAction(
             PlaybackStateCompat.CustomAction.Builder(
-                ACTION_QUIT, getString(R.string.action_cancel), R.drawable.ic_close
+                TOGGLE_SHUFFLE, getString(R.string.action_toggle_shuffle), shuffleIcon
             )
                 .build()
         )
@@ -1389,7 +1377,7 @@ class MusicService : MediaBrowserServiceCompat(),
         mediaButtonIntent.component = mediaButtonReceiverComponentName
         val mediaButtonReceiverPendingIntent = PendingIntent.getBroadcast(
             applicationContext, 0, mediaButtonIntent,
-            if (VersionUtils.hasMarshmallow()) PendingIntent.FLAG_IMMUTABLE else 0
+            PendingIntent.FLAG_IMMUTABLE
         )
         mediaSession = MediaSessionCompat(
             this,
