ce3f596ce1c9358ff8892986a4739d265b5a64a2
Dhruvit Pokharna <10249734+dhruvitpokharna@users.noreply.github.com>
2026-07-07 15:38:13 +0530

    Update AboutFragment.kt

--- app/src/main/java/code/name/monkey/retromusic/fragments/about/AboutFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/about/AboutFragment.kt
@@ -88,7 +88,7 @@ class AboutFragment : Fragment(R.layout.fragment_about), View.OnClickListener {
         ShareCompat.IntentBuilder(requireActivity())
             .setType("text/plain")
             .setChooserTitle(R.string.share_app)
-            .setText("Hey check out this cool music player at: https://github.com/effinmr/EffinMusic")
+            .setText("Hey check out this cool music player at: https://github.com/dhruvitpokharna/EffinMusic")
             .startChooser()
     }
 
