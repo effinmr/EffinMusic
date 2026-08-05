dd14b4c8b44c9b871113f246ec5adc0c3df05425
Dhruvit <effinmr@gmail.com>
2026-08-05 11:30:49 +0530

    up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         }
 
         applicationId = "code.effinmr.music"
-        versionCode = 100000
-        versionName = "1.0.0"
+        versionCode = 100001
+        versionName = "1.0.1"
 
         buildConfigField("String", "GOOGLE_PLAY_LICENSING_KEY", "\"${getProperty(getProperties("../public.properties"), "GOOGLE_PLAY_LICENSE_KEY")}\"")
     }
